bebce00ce660ef4349eb8f6ca6ec236315e14987
ItsTun <imtunhanmra@gmail.com>
2020-01-18 22:39:17 +0630

    Add movie controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'country/:name', to: 'country#index', as: :country_movie
   get 'genre/:name', to: 'genre#index', as: :genre_movie
   get 'type/:name', to: 'type#index', as: :type_movie
+  get 'movie/:name', to: 'movie#show', as: :movie_detail
 end
