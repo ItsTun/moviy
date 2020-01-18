d58693273b98a2be72f0bad6a58f2e5b00574e1e
ItsTun <imtunhanmra@gmail.com>
2020-01-18 22:30:20 +0630

    WIP

--- spec/controllers/country_controller_spec.rb
+++ spec/controllers/country_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CountryController, type: :controller do
-
+  let(:country) { FactoryBot.create :country }
+  describe 'GET #index' do
+    it 'should show all movies of country' do
+      2.times do
+        FactoryBot.create :movie, country_id: country.id
+      end
+      get :index, params: {name: country.name}
+      expect(assigns(:movies).count).to eq(2)
+    end
+  end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-
+  describe 'GET #index' do
+    it 'should show all movies' do
+      movie = FactoryBot.create :movie
+      get :index
+      byebug
+      expect(assigns(:movies).first).to eq(movie)
+    end
+  end
 end
