729c8e27af3c182b83117bb5a20a212f05226de2
ItsTun <imtunhanmra@gmail.com>
2020-02-01 14:58:03 +0630

    Add productin mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  server '157.245.147.47', user: 'root', roles: %w{app db web}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
